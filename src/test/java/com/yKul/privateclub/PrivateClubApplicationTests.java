4b889868bdc80108e549e2068e0327e241bddc09
«duKe» <dyuk0vser@yandex.ru>
2026-09-04 20:42:11 +0300

    Добавить PrivateClubApplicationTests

--- src/test/java/com/yKul/privateclub/PrivateClubApplicationTests.java
+++ src/test/java/com/yKul/privateclub/PrivateClubApplicationTests.java
@@ -1,13 +1,30 @@
 package com.yKul.privateclub;
 
+import com.yKul.privateclub.controller.GuestController;
+import com.yKul.privateclub.service.GuestService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PrivateClubApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	private GuestController guestController;
+
+	@Autowired
+	private GuestService guestService;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
+		assertThat(guestController).isNotNull();
+		assertThat(guestService).isNotNull();
 	}
-
 }
